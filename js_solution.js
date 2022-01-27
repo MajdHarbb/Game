464e4395536295411ebbfb522a3ffe5666728b50
MajdHarbb <majdwalidharb@gmail.com>
2022-01-27 16:41:19 +0200

    modified sequences

--- js_solution.js
+++ js_solution.js
@@ -4,16 +4,23 @@ var red_sound = new Audio("sounds/red.mp3");
 var wrong_sound = new Audio("sounds/wrong.mp3");
 var yellow_sound = new Audio("sounds/yellow.mp3");
 
-const sounds = [blue_sound, green_sound, red_sound, yellow_sound, wrong_sound];
+var blue = document.getElementById("blue");
+var green = document.getElementById("green");
+var red = document.getElementById("red");
+var yellow = document.getElementById("yellow");
 
-var generated_sequence=[];
-var input_sequence=[];
+
+const sounds = [green_sound, red_sound, yellow_sound, blue_sound, wrong_sound];
+
+var generated_sequence = [];
+var input_sequence = [];
+var level = 0;
 
 
 // Add event listener on keydown
 document.addEventListener("keydown", (event) => {
   StartGame();
-  });
+});
 
 
 function StartGame() {
@@ -21,34 +28,37 @@ function StartGame() {
   generated_sequence.push(random);
   Display(generated_sequence.slice(-1)[0]);
   console.log(generated_sequence.slice(-1)[0]);
-  
+
 }
 
-function Display(index){
-  if(index == 0){
+function Display(index) {
+
+  if (index == 0) {
     sounds[0].play();
-    document.getElementById("blue").classList.add("pressed");
-    setTimeout(function(){
-      document.getElementById("blue").classList.remove("pressed");
-    },250)
-    
-  }else if(index == 1){
+    green.classList.add("pressed");
+    setTimeout(function () {
+      green.classList.remove("pressed");
+    }, 200)
+
+  } else if (index == 1) {
     sounds[1].play();
-    document.getElementById("green").classList.add("pressed");
-    setTimeout(function(){
-      document.getElementById("green").classList.remove("pressed");
-    },250)
-  }else if(index == 2){
+    red.classList.add("pressed");
+    setTimeout(function () {
+      red.classList.remove("pressed");
+    }, 200)
+  } else if (index == 2) {
     sounds[2].play();
-    document.getElementById("red").classList.add("pressed");
-    setTimeout(function(){
-      document.getElementById("red").classList.remove("pressed");
-    },250)
-  }else if(index == 3){
+    yellow.classList.add("pressed");
+    setTimeout(function () {
+      yellow.classList.remove("pressed");
+    }, 200)
+  } else if (index == 3) {
     sounds[3].play();
-    document.getElementById("yellow").classList.add("pressed");
-    setTimeout(function(){
-      document.getElementById("yellow").classList.remove("pressed");
-    },250)
+    blue.classList.add("pressed");
+    setTimeout(function () {
+      blue.classList.remove("pressed");
+    }, 200)
   }
 }
+
+   
