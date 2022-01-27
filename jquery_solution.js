5edf3a13fc91ac320394a18eeebb5f842360d759
MajdHarbb <majdwalidharb@gmail.com>
2022-01-27 19:42:21 +0200

    display sounds and effects on click

--- jquery_solution.js
+++ jquery_solution.js
@@ -23,6 +23,23 @@ var level = 0;
 /* Game Starts on keyboard press */
 $(document).on("keydown", function(event){
   StartGame();
+  /** Onclick call Display with parameter */
+  green.click(function(){
+    Display(0);
+  })
+  red.click(function(){
+    Display(1);
+  })
+  yellow.click(function(){
+    Display(2);
+  })
+  blue.click(function(){
+    Display(3);
+  })
+  
+
+
+
 });
 
 function StartGame(){
@@ -30,3 +47,38 @@ function StartGame(){
   generated_sequence.push(random);
 
 } 
+
+function Display(index){
+  if (index == 0) {
+    sounds[0].play();
+    green.addClass("pressed");
+    setTimeout(function () {
+      green.removeClass("pressed");
+    }, 200)
+
+
+  } else if (index == 1) {
+    sounds[1].play();
+    red.addClass("pressed");
+    setTimeout(function () {
+      red.removeClass("pressed");
+    }, 200)
+
+
+  } else if (index == 2) {
+    sounds[2].play();
+    yellow.addClass("pressed");
+    setTimeout(function () {
+      yellow.removeClass("pressed");
+    }, 200)
+
+
+  } else if (index == 3) {
+    sounds[3].play();
+    blue.addClass("pressed");
+    setTimeout(function () {
+      blue.removeClass("pressed");
+    }, 200)
+  }
+}
+  
