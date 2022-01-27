5fce074b7477a06d988903f017fe054831669589
MajdHarbb <majdwalidharb@gmail.com>
2022-01-27 16:06:38 +0200

    Random Sequences Sounds

--- js_solution.js
+++ js_solution.js
@@ -1,20 +1,36 @@
+var blue_sound = new Audio("sounds/blue.mp3");
+var green_sound = new Audio("sounds/green.mp3");
+var red_sound = new Audio("sounds/red.mp3");
+var wrong_sound = new Audio("sounds/wrong.mp3");
+var yellow_sound = new Audio("sounds/yellow.mp3");
+
+const sounds = [blue_sound, green_sound, red_sound, yellow_sound, wrong_sound];
+
+var generated_sequence=[];
+var input_sequence=[];
+
+
 // Add event listener on keydown
-document.addEventListener('keydown', (event) => {
-  startgame();
-}, false);
+document.addEventListener("keydown", (event) => {
+  StartGame();
+  });
 
-function startgame(){
-  var blue_sound = new Audio("sounds/blue.mp3");
-    var green_sound = new Audio("sounds/green.mp3");
-    var red_sound = new Audio("sounds/red.mp3");
-    var wrong_sound = new Audio("sounds/wrong.mp3");
-    var yellow_sound = new Audio("sounds/yellow.mp3");
-    
-    const colors = [blue_sound,green_sound,red_sound,yellow_sound,wrong_sound];
+function StartGame() {
+  var random = Math.floor(Math.random() * 4);
+  generated_sequence.push(random);
+  Display(generated_sequence.slice(-1)[0]);
+  console.log(generated_sequence.slice(-1)[0]);
+  
+}
 
-    var random = Math.floor(Math.random()*4+1);
-    
-    console.log(colors[random]);
-    colors[0].play();
-    colors[4].play();
+function Display(index){
+  if(index == 0){
+    sounds[0].play();
+  }else if(index == 1){
+    sounds[1].play();
+  }else if(index == 2){
+    sounds[2].play();
+  }else if(index == 3){
+    sounds[3].play();
+  }
 }
