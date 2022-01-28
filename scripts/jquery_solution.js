746c32dc7d32ccb99e5c3b8ef43058f4d3409b62
MajdHarbb <majdwalidharb@gmail.com>
2022-01-28 22:25:11 +0200

    files

--- scripts/jquery_solution.js
+++ scripts/jquery_solution.js
@@ -9,7 +9,7 @@ var yellow_sound = new Audio("sounds/yellow.mp3");
 var blue = $("#blue");
 var green = $("#green");
 var red = $("#red");
-var yellow =$("#yellow");
+var yellow = $("#yellow");
 
 
 /* array of audio files */
@@ -19,80 +19,103 @@ const sounds = [green_sound, red_sound, yellow_sound, blue_sound, wrong_sound];
 var generated_sequence = [];
 var input_sequence = [];
 var level = 0;
+game_on = false;
 
 /* Game Starts on keyboard press */
-$(document).on("keydown", function(event){
-  StartGame();
-  /** Onclick call Display with parameter */
-  green.click(function(){
-    Display(0);
-    input_sequence.push(0);
-    if(input_sequence[input_sequence.length-1] == generated_sequence[input_sequence.length-1]){
-      if (generated_sequence.length == input_sequence.length) {
-        setTimeout(function () { StartGame();}, 1500);
-      }
-    }else{GameOver();}
-  })
-  red.click(function(){
-    Display(1);
-    input_sequence.push(1);
-    if(input_sequence[input_sequence.length-1] == generated_sequence[input_sequence.length-1]){
-      if (generated_sequence.length == input_sequence.length) {
-        setTimeout(function () { StartGame();}, 1500);
-      }
-    }else{GameOver();}
-  })
-  yellow.click(function(){
-    Display(2);
-    input_sequence.push(2);
-    if(input_sequence[input_sequence.length-1] == generated_sequence[input_sequence.length-1]){
-      if (generated_sequence.length == input_sequence.length) {
-        setTimeout(function () { StartGame();}, 1500);
-      }
-    }else{GameOver();}
-  })
-  blue.click(function(){
-    Display(3);
-    input_sequence.push(3);
-    if(input_sequence[input_sequence.length-1] == generated_sequence[input_sequence.length-1]){
-      if (generated_sequence.length == input_sequence.length) {
-        setTimeout(function () { StartGame();}, 1500);
-      }
-    }else{GameOver();}
-  })
-  
+$(document).on("keydown", function (event) {
+  if (game_on == false) {
+    game_on = true;
+    StartGame();
+    /** Onclick call Display with parameter */
+    green.click(function () {
+      Display(0);
+      input_sequence.push(0);
+      checkInput(0);
+    })
+    red.click(function () {
+      Display(1);
+      input_sequence.push(1);
+      checkInput(1);
+    })
+    yellow.click(function () {
+      Display(2);
+      input_sequence.push(2);
+      checkInput(2)
+    })
+    blue.click(function () {
+      Display(3);
+      input_sequence.push(3);
+      checkInput(3);
+    })
+
+  }
+
 
 
 
 });
 /** Make random buttons glow  */
-function StartGame(){
-  var random = Math.floor(Math.random()*4);
+function StartGame() {
+  var random = Math.floor(Math.random() * 4);
   generated_sequence.push(random);
-  var displayed = generated_sequence[generated_sequence.length-1];
-  Display(displayed);
+  var displayed = generated_sequence[generated_sequence.length - 1];
+  GenFade(displayed);
   console.log(generated_sequence);
   level = level + 1;
-  document.getElementById("title").innerText = "Level " +level;
+  document.getElementById("title").innerText = "Level " + level;
   $("#title").text(`Level: ${level}`);
-  input_sequence=[];
+  input_sequence = [];
 
-} 
+}
 
 /* Play game over sound and red background + reset defualts (level and sequence) */
 function GameOver() {
   sounds[4].play();
   $("body").addClass("game-over");
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
-    $("#title").text("Game Over! Press any key to Restart");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 200);
+  $("#title").text("Game Over! Press any key to Restart");
   level = 0;
   generated_sequence = [];
+  game_on = fasle;
 }
 
+function GenFade(index) {
+  if (index == 0) {
+    sounds[0].play();
+    green.style.opacity = 0;
+    setTimeout(function () {
+      green.style.opacity = 1;
+    }, 200);
 
-function Display(index){
+
+  } else if (index == 1) {
+    sounds[1].play();
+    red.style.opacity = 0;
+    setTimeout(function () {
+      red.style.opacity = 1;
+    }, 200);
+
+
+  } else if (index == 2) {
+    sounds[2].play();
+    yellow.style.opacity = 0;
+    setTimeout(function () {
+      yellow.style.opacity = 1;
+    }, 200);
+
+
+  } else if (index == 3) {
+    sounds[3].play();
+    blue.style.opacity = 0;
+    setTimeout(function () {
+      blue.style.opacity = 1;
+    }, 200);
+  }
+}
+
+function Display(index) {
   if (index == 0) {
     sounds[0].play();
     green.addClass("pressed");
@@ -125,4 +148,14 @@ function Display(index){
     }, 200)
   }
 }
-  
+function checkInput(index) {
+  if (generated_sequence[index] == input_sequence[index]) {
+    if (input_sequence.length == generated_sequence.length) {
+      setTimeout(function() {
+        StartGame();
+      }, 1000);
+    }
+  } else {
+    GameOver();
+  }
+}
